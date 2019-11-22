a1e9d3e85ee755c620563e41052f77d3f611325e
jeff-gosselin <jgoz.email@gmail.com>
2019-11-22 00:27:31 -0500

    Laying out components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from "react";
 
 // Components
 import Nav from "./components/Nav";
+import Greeting from "./components/Greeting";
+import SocialMedia from "./components/SocialMedia";
+import Contact from "./components/Contact";
+import Search from "./components/Search";
+import Image from "./components/Image";
+import Gallery from "./components/Gallery";
 
 // Stylesheet
 import "./css/App.css";
@@ -10,6 +16,12 @@ function App() {
   return (
     <div className="App">
       <Nav />
+      <Greeting />
+      <SocialMedia />
+      <Contact />
+      <Search />
+      <Image />
+      <Gallery />
     </div>
   );
 }
