f9cac7a21d9abaaf6c044f727366fee4b3997e35
jeff-gosselin <jgoz.email@gmail.com>
2019-11-22 17:38:38 -0500

    layout wip

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -6,6 +6,7 @@ import "../css/AboutUs.scss";
 const AboutUs = () => {
   return (
     <div className="about">
+      <button className="about-toggle">^</button>
       <h1>AboutUs</h1>
     </div>
   );
--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -6,6 +6,7 @@ import "../css/ContactUs.scss";
 const ContactUs = () => {
   return (
     <div className="contact">
+      <button className="contact-toggle">^</button>
       <h1>ContactUs</h1>
     </div>
   );
