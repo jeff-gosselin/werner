141330256456f30c751e4c200cb14d29a1c0abdd
jeff-gosselin <jgoz.email@gmail.com>
2019-11-24 09:49:29 -0500

    creating expandable contact form

--- src/components/ContactContent.js
+++ src/components/ContactContent.js
@@ -1,9 +1,12 @@
 import React from "react";
 
+// Stylesheet
+import "../css/ContactUs.scss";
+
 // This component goes inside of the ContactUs component
 const ContactContent = () => {
   return (
-    <div>
+    <div className="contact-form">
       <h1>Contact Content</h1>
     </div>
   );
