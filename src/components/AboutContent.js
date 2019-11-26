315c73fd04c778ad07a65fb2bb522a83e8aa4a3e
jeff-gosselin <jgoz.email@gmail.com>
2019-11-26 10:49:23 -0500

    working on about content wip

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -8,14 +8,22 @@ const AboutContent = () => {
   return (
     <div className="drawer-content">
       <div className="about-content">
-        <div className="about-content-image">
-          <img src="" alt="" />
-        </div>
+        <div className="about-content-image"></div>
         <h3>About Content</h3>
         <p>
           Commodo sit consectetur commodo cupidatat officia quis dolor ex. Culpa
           nostrud anim duis ex labore culpa culpa occaecat est reprehenderit
           dolor. Reprehenderit consectetur in cillum enim laboris.
+        </p>
+
+        <h3>More Content</h3>
+        <p>
+          Quis velit consequat id consequat labore nostrud qui quis non. Id
+          laboris ullamco amet cillum officia ut. Ipsum sunt id occaecat
+          exercitation aute sint. Irure pariatur ea Lorem esse magna sint id sit
+          irure qui tempor ut nisi. Eiusmod culpa esse reprehenderit fugiat
+          voluptate. Voluptate veniam Lorem culpa officia irure reprehenderit
+          reprehenderit deserunt.
         </p>
 
         <h3>More Content</h3>
