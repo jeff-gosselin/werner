dc70375c8e29e0857440b21a7bf7cfafde104c66
jeff-gosselin <jgoz.email@gmail.com>
2019-12-09 15:23:37 -0500

    adding nav for gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,4 +1,5 @@
 import React from "react";
+import GalleryNav from "./GalleryNav";
 
 // Components
 import Swatch from "./Swatch";
@@ -7,8 +8,15 @@ import Swatch from "./Swatch";
 import "../css/Gallery.scss";
 
 const Gallery = props => {
+  console.log("Props", props.products);
+
+  let ceramics = props.products.filter(product => product.type === "ceramic");
+  let stone = props.products.filter(product => product.type === "stone");
+  let wood = props.products.filter(product => product.type === "wood");
+
   return (
     <div className={props.toggle ? "gallery show" : "gallery hide"}>
+      <GalleryNav />
       <h1>{props.products[0].name}</h1>
     </div>
   );
---
+++ src/components/GalleryNav.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GalleryNav = () => {
+  return (
+    <div>
+      <h1>Gallery Navigation</h1>
+    </div>
+  );
+};
+
+export default GalleryNav;
